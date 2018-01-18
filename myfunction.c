5e47ab4e84dc29eeb55ebb5fc5dca127d935939b
OriBenZaken <orinetanel1@gmail.com>
2018-01-18 14:07:33 +0200

    almost done

--- myfunction.c
+++ myfunction.c
@@ -26,17 +26,16 @@ typedef struct {
 /**
  * Blurring the image.
  * since we know for sure that every element in the kernel is 1, the kernel scale is 9
- * and the kernel size is 3 we gave up on passing this argument to the function and we refer
+ * and the kernel size is 3 we gave up on passing this arguments to the function and we refer
  * this values as constants. We performed loop unrolling to the whole process so we have one 2 for nested
  * loops, in each iteration we calculate two pixels or one pixel, depends if m is even or odd.
  */
-void blurImg(int dim, pixel *src, pixel *dst) {
+void blurImg(pixel *src, pixel *dst) {
 	int i, j;
     // kernel size is always 3, therefore 3/2 is always 1.
 	i = 1;
-    // calculate dim - 1 once instead of every iteration in the head of the for-loop.
-	int limit = dim - 1;
-    //int weight = 1;
+    // calculate m - 1 once instead of every iteration in the head of the for-loop.
+	int limit = m - 1;
     // since the loop unrooling, and the fact that we calculating the value of sum
     // in one command and not accumulating the value of sum with each inner iterations it is
     // enough to define sum and current_pixel, too, once here and give up on initializing sum.
@@ -46,19 +45,19 @@ void blurImg(int dim, pixel *src, pixel *dst) {
     if (m % 2 == 0) { // m is even
         for (; i < limit; ++i) {
             j =  1;
-            int index = calcIndex(i, j, dim);
+            int index = calcIndex(i, j, m);
             for (; j < limit; ++j) {
                 // calculating first pixel - calculating in each iteration two pixels
-                register int index1 = calcIndex(i-1, j - 1, dim);
+                register int index1 = calcIndex(i-1, j - 1, m);
                 pixel p1 = src[index1];
                 pixel p2 = src[index1 + 1];
                 pixel p3 = src[index1 + 2];
-                pixel p4 = src[index1 + dim];
-                pixel p5 = src[index1 + dim + 1];
-                pixel p6 = src[index1 + dim + 2];
-                pixel p7 = src[index1 + dim + dim];
-                pixel p8 = src[index1 + dim + dim + 1];
-                pixel p9 = src[index1 + dim + dim + 2];
+                pixel p4 = src[index1 + m];
+                pixel p5 = src[index1 + m + 1];
+                pixel p6 = src[index1 + m + 2];
+                pixel p7 = src[index1 + m + m];
+                pixel p8 = src[index1 + m + m + 1];
+                pixel p9 = src[index1 + m + m + 2];
 
                 sum.red = ((int) p1.red) + ((int) p2.red) + ((int) p3.red)+
                           ((int) p4.red) + ((int) p5.red) + ((int) p6.red) +
@@ -90,16 +89,16 @@ void blurImg(int dim, pixel *src, pixel *dst) {
 
                 // calculating second pixel
                 ++j;
-                index1 = calcIndex(i-1, j - 1, dim);
+                index1 = calcIndex(i-1, j - 1, m);
                 p1 = src[index1];
                 p2 = src[index1 + 1];
                 p3 = src[index1 + 2];
-                p4 = src[index1 + dim];
-                p5 = src[index1 + dim + 1];
-                p6 = src[index1 + dim + 2];
-                p7 = src[index1 + dim + dim];
-                p8 = src[index1 + dim + dim + 1];
-                p9 = src[index1 + dim + dim + 2];
+                p4 = src[index1 + m];
+                p5 = src[index1 + m + 1];
+                p6 = src[index1 + m + 2];
+                p7 = src[index1 + m + m];
+                p8 = src[index1 + m + m + 1];
+                p9 = src[index1 + m + m + 2];
 
                 sum.red = ((int) p1.red) + ((int) p2.red) + ((int) p3.red)+
                           ((int) p4.red) + ((int) p5.red) + ((int) p6.red) +
@@ -110,7 +109,6 @@ void blurImg(int dim, pixel *src, pixel *dst) {
                 sum.blue = ((int) p1.blue) + ((int) p2.blue) + ((int) p3.blue) +
                            ((int) p4.blue) + ((int) p5.blue) + ((int) p6.blue) +
                            ((int) p7.blue) + ((int) p8.blue) + ((int) p9.blue);
-                //sum.num += 3;
 
                 // assign kernel's result to pixel at [i,j]
                 // divide by kernel's weight
@@ -130,19 +128,19 @@ void blurImg(int dim, pixel *src, pixel *dst) {
     } else { // m is odd - calculating in each iteration a single pixel
         for (; i < limit; ++i) {
             j =  1;
-            int index = calcIndex(i, j, dim);
+            int index = calcIndex(i, j, m);
             for (; j < limit; ++j) {
                 // calculating first pixel
-                register int index1 = calcIndex(i - 1, j - 1, dim);
+                register int index1 = calcIndex(i - 1, j - 1, m);
                 pixel p1 = src[index1];
                 pixel p2 = src[index1 + 1];
                 pixel p3 = src[index1 + 2];
-                pixel p4 = src[index1 + dim];
-                pixel p5 = src[index1 + dim + 1];
-                pixel p6 = src[index1 + dim + 2];
-                pixel p7 = src[index1 + dim + dim];
-                pixel p8 = src[index1 + dim + dim + 1];
-                pixel p9 = src[index1 + dim + dim + 2];
+                pixel p4 = src[index1 + m];
+                pixel p5 = src[index1 + m + 1];
+                pixel p6 = src[index1 + m + 2];
+                pixel p7 = src[index1 + m + m];
+                pixel p8 = src[index1 + m + m + 1];
+                pixel p9 = src[index1 + m + m + 2];
 
                 sum.red = ((int) p1.red) + ((int) p2.red) + ((int) p3.red)+
                           ((int) p4.red) + ((int) p5.red) + ((int) p6.red) +
@@ -176,38 +174,41 @@ void blurImg(int dim, pixel *src, pixel *dst) {
 }
 
 /**
- * Blurring the image.
+ * Sharping the image.
  * since we know for sure that every element in the kernel is -1, except kernel[1][1] = 9, the kernel scale is 1
- * and the kernel size is 3 we gave up on passing this argument to the function and we refer
+ * and the kernel size is 3 we gave up on passing this arguments to the function and we refer
  * this values as constants. We performed loop unrolling to the whole process so we have one 2 for nested
  * loops, in each iteration we calculate two pixels or one pixel, depends if m is even or odd.
  */
-void sharpImg(int dim, pixel *src, pixel *dst) {
-    //todo: optimizations here
+void sharpImg(pixel *src, pixel *dst) {
     int i, j;
+    // kernel size is always 3, therefore 3/2 is always 1.
     i = 1;
-    int baseJ = i; // j = kernelSize / 2;
-    int limit = dim - 1;
+    // calculate m - 1 once instead of every iteration in the head of the for-loop.
+    int limit = m - 1;
     //todo: changed i++ to ++i
 
-    //todo: out of the loop
+    // since the loop unrooling, and the fact that we calculating the value of sum
+    // in one command and not accumulating the value of sum with each inner iterations it is
+    // enough to define sum and current_pixel, too, once here and give up on initializing sum.
     pixel_sum sum;
     pixel current_pixel;
-    if (m % 2 == 0) {
+    // i++ was changed to ++i since it's more fast.
+    if (m % 2 == 0) { // m is even
         for (; i < limit; ++i) {
-            j =  baseJ;
-            int index = calcIndex(i, j, dim);
+            j =  1;
+            int index = calcIndex(i, j, m);
             for (; j < limit; ++j) {
-                register int index1 = calcIndex(i - 1, j - 1, dim);
+                register int index1 = calcIndex(i - 1, j - 1, m);
                 pixel p1 = src[index1];
                 pixel p2 = src[index1 + 1];
                 pixel p3 = src[index1 + 2];
-                pixel p4 = src[index1 + dim];
-                pixel p5 = src[index1 + dim + 1];
-                pixel p6 = src[index1 + dim + 2];
-                pixel p7 = src[index1 + dim + dim];
-                pixel p8 = src[index1 + dim + dim + 1];
-                pixel p9 = src[index1 + dim + dim + 2];
+                pixel p4 = src[index1 + m];
+                pixel p5 = src[index1 + m + 1];
+                pixel p6 = src[index1 + m + 2];
+                pixel p7 = src[index1 + m + m];
+                pixel p8 = src[index1 + m + m + 1];
+                pixel p9 = src[index1 + m + m + 2];
 
                 sum.red = -(((int) p1.red) + ((int) p2.red) + ((int) p3.red)+
                             ((int) p4.red) +  ((int) p6.red) +
@@ -220,13 +221,7 @@ void sharpImg(int dim, pixel *src, pixel *dst) {
                              ((int) p7.blue) + ((int) p8.blue) + ((int) p9.blue)) + Mult9(((int) p5.blue));
 
                 // assign kernel's result to pixel at [i,j]
-                //assign_sum_to_pixel(&current_pixel, sum, kernelScale);
-
-                // todo: מיותר מכיוון ש סקייל = 1
-/*            // divide by kernel's weight
-            sum.red = sum.red / kernelScale;
-            sum.green = sum.green / kernelScale;
-            sum.blue = sum.blue / kernelScale;*/
+                // kernel scale in this case is 1 so there is no need to divide the sum by 1
 
                 // truncate each pixel's color values to match the range [0,255]
                 current_pixel.red = (unsigned char) (min(max(sum.red, 0), 255));
@@ -238,16 +233,16 @@ void sharpImg(int dim, pixel *src, pixel *dst) {
                 ++index;
 
                 ++j;
-                index1 = calcIndex(i-1, j - 1, dim);
+                index1 = calcIndex(i-1, j - 1, m);
                 p1 = src[index1];
                 p2 = src[index1 + 1];
                 p3 = src[index1 + 2];
-                p4 = src[index1 + dim];
-                p5 = src[index1 + dim + 1];
-                p6 = src[index1 + dim + 2];
-                p7 = src[index1 + dim + dim];
-                p8 = src[index1 + dim + dim + 1];
-                p9 = src[index1 + dim + dim + 2];
+                p4 = src[index1 + m];
+                p5 = src[index1 + m + 1];
+                p6 = src[index1 + m + 2];
+                p7 = src[index1 + m + m];
+                p8 = src[index1 + m + m + 1];
+                p9 = src[index1 + m + m + 2];
 
                 sum.red = -(((int) p1.red) + ((int) p2.red) + ((int) p3.red)+
                             ((int) p4.red) +  ((int) p6.red) +
@@ -260,14 +255,6 @@ void sharpImg(int dim, pixel *src, pixel *dst) {
                              ((int) p7.blue) + ((int) p8.blue) + ((int) p9.blue)) + Mult9(((int) p5.blue));
 
                 // assign kernel's result to pixel at [i,j]
-                //assign_sum_to_pixel(&current_pixel, sum, kernelScale);
-
-                // todo: מיותר מכיוון ש סקייל = 1
-/*            // divide by kernel's weight
-            sum.red = sum.red / kernelScale;
-            sum.green = sum.green / kernelScale;
-            sum.blue = sum.blue / kernelScale;*/
-
                 // truncate each pixel's color values to match the range [0,255]
                 current_pixel.red = (unsigned char) (min(max(sum.red, 0), 255));
                 current_pixel.green = (unsigned char) (min(max(sum.green, 0), 255));
@@ -278,21 +265,21 @@ void sharpImg(int dim, pixel *src, pixel *dst) {
                 ++index;
             }
         }
-    } else {
+    } else {    // m is odd - calculating in each iteration a single pixel
         for (; i < limit; ++i) {
-            j =  baseJ;
-            int index = calcIndex(i, j, dim);
+            j =  1;
+            int index = calcIndex(i, j, m);
             for (; j < limit; ++j) {
-                register int index1 = calcIndex(i - 1, j - 1, dim);
+                register int index1 = calcIndex(i - 1, j - 1, m);
                 pixel p1 = src[index1];
                 pixel p2 = src[index1 + 1];
                 pixel p3 = src[index1 + 2];
-                pixel p4 = src[index1 + dim];
-                pixel p5 = src[index1 + dim + 1];
-                pixel p6 = src[index1 + dim + 2];
-                pixel p7 = src[index1 + dim + dim];
-                pixel p8 = src[index1 + dim + dim + 1];
-                pixel p9 = src[index1 + dim + dim + 2];
+                pixel p4 = src[index1 + m];
+                pixel p5 = src[index1 + m + 1];
+                pixel p6 = src[index1 + m + 2];
+                pixel p7 = src[index1 + m + m];
+                pixel p8 = src[index1 + m + m + 1];
+                pixel p9 = src[index1 + m + m + 2];
 
                 sum.red = -(((int) p1.red) + ((int) p2.red) + ((int) p3.red)+
                             ((int) p4.red) +  ((int) p6.red) +
@@ -305,19 +292,10 @@ void sharpImg(int dim, pixel *src, pixel *dst) {
                              ((int) p7.blue) + ((int) p8.blue) + ((int) p9.blue)) + Mult9(((int) p5.blue));
 
                 // assign kernel's result to pixel at [i,j]
-                //assign_sum_to_pixel(&current_pixel, sum, kernelScale);
-
-                // todo: מיותר מכיוון ש סקייל = 1
-/*            // divide by kernel's weight
-            sum.red = sum.red / kernelScale;
-            sum.green = sum.green / kernelScale;
-            sum.blue = sum.blue / kernelScale;*/
-
                 // truncate each pixel's color values to match the range [0,255]
                 current_pixel.red = (unsigned char) (min(max(sum.red, 0), 255));
                 current_pixel.green = (unsigned char) (min(max(sum.green, 0), 255));
                 current_pixel.blue = (unsigned char) (min(max(sum.blue, 0), 255));
-                //
 
                 dst[index] = current_pixel;
                 ++index;
@@ -344,9 +322,9 @@ void doConvolution(Image *image, int kernelScale) {
 
     //copyPixels(pixelsImg, backupOrg);
     if (kernelScale == 9) {
-        blurImg(m, backupOrg, pixelsImg);
+        blurImg(backupOrg, pixelsImg);
     } else {
-        sharpImg(m, backupOrg, pixelsImg);
+        sharpImg(backupOrg, pixelsImg);
 
     }
 
@@ -359,31 +337,18 @@ void doConvolution(Image *image, int kernelScale) {
 
 
 
-
+/**
+ * We found it not necessary allocating blurKernel and sharpKernel since their values are known.
+ * Also we decreases the number of arguments to doConvolution.
+ */
 void myfunction(Image *image, char* srcImgpName, char* blurRsltImgName, char* sharpRsltImgName) {
-
-    /*
-    * [1, 1, 1]
-    * [1, 1, 1]
-    * [1, 1, 1]
-    */
-    //int blurKernel[3][3] = {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
-
-    /*
-    * [-1, -1, -1]
-    * [-1, 9, -1]
-    * [-1, -1, -1]
-    */
-    //int sharpKernel[3][3] = {{-1,-1,-1},{-1,9,-1},{-1,-1,-1}};
 
     // blur image
     doConvolution(image, 9);
 
     // write result image to file
     writeBMP(image, srcImgpName, blurRsltImgName);
-
-    //smooth(m, backupOrg, pixelsImg, kernelSize, sharpKernel, 1);
-
+    
     // sharpen the resulting image
     doConvolution(image, 1);
 
